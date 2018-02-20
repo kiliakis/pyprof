5555d4a22481f0d973d2696aa452d02df6095c0e
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2018-02-20 09:51:41 +0100

    Added support for python 2.7

--- timing.py
+++ timing.py
@@ -1,5 +1,8 @@
 from functools import wraps
-import time
+try:
+    from time import perf_counter as timer
+except ImportError:
+    from time import time as timer
 import inspect
 import numpy as np
 
@@ -16,7 +19,7 @@ lp = None
 # 'timing'
 
 
-def timeit(filename='', classname='', exclude=False):
+def timeit(filename='', classname='', key='', exclude=False):
     global times, excluded, disabled, mode
 
     def decorator(f):
@@ -25,29 +28,31 @@ def timeit(filename='', classname='', exclude=False):
             if mode == 'disabled':
                 return f(*args, **kw)
             elif mode == 'timing':
-                ts = time.time()
+                ts = timer()
                 result = f(*args, **kw)
-                te = time.time()
+                te = timer()
 
-                if not filename:
-                    _filename = inspect.stack()[1].filename.split('/')[-1]
+                if key == '':
+                    if not filename:
+                        _filename = inspect.stack()[1].filename.split('/')[-1]
+                    else:
+                        _filename = filename
+                    _key = _filename + '.' + f.__name__
+                    if classname:
+                        _key = classname + '.' + _key
                 else:
-                    _filename = filename
-                key = _filename + '.' + f.__name__
-                if classname:
-                    key = classname + '.' + key
-
+                    _key = key
                 # key = filename
-                if(key not in times):
-                    times[key] = []
+                if(_key not in times):
+                    times[_key] = []
                     if exclude:
-                        excluded.append(key)
+                        excluded.append(_key)
 
-                times[key].append((te - ts) * 1000)
+                times[_key].append((te - ts) * 1000)
 
                 if 'lib_time' not in times:
                     times['lib_time'] = []
-                times['lib_time'].append((time.time() - te) * 1000)
+                times['lib_time'].append((timer() - te) * 1000)
 
                 return result
             elif mode == 'line_profiler':
@@ -59,7 +64,8 @@ def timeit(filename='', classname='', exclude=False):
                 result = lp_wrapper(*args, **kw)
                 return result
             else:
-                raise RuntimeError('[timing.py:timeit] mode: %s not available' % mode)
+                raise RuntimeError(
+                    '[timing.py:timeit] mode: %s not available' % mode)
         return timed
     return decorator
 
@@ -71,7 +77,7 @@ class timed_region:
 
     def __init__(self, region_name='', is_child_function=False):
 
-        ls = time.time()
+        ls = timer()
         global times, excluded, mode
 
         if mode == 'disabled':
@@ -92,28 +98,30 @@ class timed_region:
 
             if 'lib_time' not in times:
                 times['lib_time'] = []
-            times['lib_time'].append((time.time() - ls) * 1000)
+            times['lib_time'].append((timer() - ls) * 1000)
         else:
-            raise RuntimeError('[timing:timed_region] mode: %s not available' % mode)
+            raise RuntimeError(
+                '[timing:timed_region] mode: %s not available' % mode)
 
     def __enter__(self):
-        ls = time.time()
+        ls = timer()
         global times, excluded, mode
 
         if mode == 'disabled':
             return self
         elif mode == 'timing':
 
-            times['lib_time'].append((time.time() - ls) * 1000)
+            times['lib_time'].append((timer() - ls) * 1000)
 
-            self.ts = time.time()
+            self.ts = timer()
 
             return self
         else:
-            raise RuntimeError('[timing:timed_region] mode: %s not available' % mode)
+            raise RuntimeError(
+                '[timing:timed_region] mode: %s not available' % mode)
 
     def __exit__(self, type, value, traceback):
-        te = time.time()
+        te = timer()
         global times, excluded, mode
 
         if mode == 'disabled':
@@ -121,17 +129,19 @@ class timed_region:
         elif mode == 'timing':
             times[self.key].append((te - self.ts) * 1000)
 
-            times['lib_time'].append((time.time() - te) * 1000)
+            times['lib_time'].append((timer() - te) * 1000)
             return
         else:
-            raise RuntimeError('[timing:timed_region] mode: %s not available' % mode)
+            raise RuntimeError(
+                '[timing:timed_region] mode: %s not available' % mode)
+
 
 def start_timing(funcname=''):
     global func_stack, start_time_stack, disabled, times, mode
     if mode == 'disabled':
         return
     elif mode == 'timing':
-        ts = time.time()
+        ts = timer()
         if funcname:
             key = funcname
         else:
@@ -139,21 +149,23 @@ def start_timing(funcname=''):
             key = parent.filename.split('/')[-1]
             key = key + ':' + parent.lineno
         func_stack.append(key)
-        start_time_stack.append(time.time())
+        start_time_stack.append(timer())
         if 'lib_time' not in times:
             times['lib_time'] = []
-        times['lib_time'].append((time.time() - ts) * 1000)
+        times['lib_time'].append((timer() - ts) * 1000)
     else:
-        raise RuntimeError('[timing:timed_region] mode: %s not available' % mode)
+        raise RuntimeError(
+            '[timing:timed_region] mode: %s not available' % mode)
+
 
 def stop_timing(exclude=False):
     global times, start_time_stack, func_stack, excluded, mode
     if mode == 'disabled':
         return
     elif mode == 'timing':
-        ts = time.time()
+        ts = timer()
 
-        elapsed = (time.time() - start_time_stack.pop()) * 1000
+        elapsed = (timer() - start_time_stack.pop()) * 1000
         key = func_stack.pop()
         if(key not in times):
             times[key] = []
@@ -162,9 +174,11 @@ def stop_timing(exclude=False):
         times[key].append(elapsed)
         if 'lib_time' not in times:
             times['lib_time'] = []
-        times['lib_time'].append((time.time() - ts) * 1000)
+        times['lib_time'].append((timer() - ts) * 1000)
     else:
-        raise RuntimeError('[timing:timed_region] mode: %s not available' % mode)
+        raise RuntimeError(
+            '[timing:timed_region] mode: %s not available' % mode)
+
 
 def report(skip=0, total_time=None):
     global times, excluded, mode
@@ -187,7 +201,8 @@ def report(skip=0, total_time=None):
         otherPercent = 100.0
         otherTime = _total_time
 
-        print('function\tcounter\taverage_value\tstd(%)\tcalls\tglobal_percentage')
+        print(
+            'function\ttotal_time(sec)\ttime_per_call(ms)\tstd(%)\tcalls\tglobal_percentage')
         for k, v in sorted(times.items()):
 
             if k == 'lib_time':
@@ -202,13 +217,23 @@ def report(skip=0, total_time=None):
                 otherPercent -= vGPercent
                 otherTime -= vSum
 
-            print('%s\t%s\t%.3lf\t%.2lf\t%d\t%.2lf'
-                  % (k, 'times(ms)', vMean, 100.0 * vStd / vMean,
+            print('%s\t%.3lf\t%.3lf\t%.2lf\t%d\t%.2lf'
+                  % (k, vSum/1000., vMean, 100.0 * vStd / vMean,
                      len(v), vGPercent))
 
-        print('%s\t%s\t%.3lf\t%.2lf\t%d\t%.2lf'
-              % ('Other', 'times(ms)', otherTime, 0.0, 1, otherPercent))
+        print('%s\t%.3lf\t%.3lf\t%.2lf\t%d\t%.2lf'
+              % ('Other', otherTime/1000., otherTime, 0.0, 1, otherPercent))
     elif mode == 'line_profiler':
         lp.print_stats()
     else:
         raise RuntimeError('[timing:report] mode: %s not available' % mode)
+
+
+def reset():
+    global times, start_time_stack, func_stack, excluded, mode, lp
+    times = {}
+    start_time_stack = []
+    func_stack = []
+    excluded = ['lib_time']
+    mode = 'timing'
+    lp = None
